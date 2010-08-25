3830780b93aaf43c6384cd2e81e24c3801e2c4e8
ekarulf <erik@karulf.com>
2010-08-25 00:27:59 -0700

    Checking in a test suite for testing the library

--- FortAwesomeUtilTest/Properties/AssemblyInfo.cs
+++ FortAwesomeUtilTest/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("FortAwesomeUtil")]
+[assembly: AssemblyTitle("FortAwesomeUtilTest")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("FortAwesomeUtil")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2009")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FortAwesomeUtilTest")]
+[assembly: AssemblyCopyright("Copyright ©  2010")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("15ef5e91-8375-4bc3-974c-f13d04202138")]
+[assembly: Guid("bddefd3d-854c-4558-b5fc-bf5beeb1580c")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -31,8 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-
-[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("FortAwesomeUtilTest")]
---
+++ FortAwesomeUtilTest/WebserverTest.cs
@@ -0,0 +1,106 @@
+﻿using FortAwesomeUtil.Webserver;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Net;
+using FortAwesomeUtil.Webserver.Framework;
+
+namespace FortAwesomeUtilTest
+{
+    class DummyWebservice: Webservice
+    {
+        [Path("/")]
+        [Path("basic/")]
+        public void BasicMethod(HttpListenerContext context)
+        {
+            // stub
+        }
+
+        [Path("argument/:id/")]
+        public void ArgumentMethod(HttpListenerContext context, int id)
+        {
+            // stub
+        }
+    }
+    
+    /// <summary>
+    ///This is a test class for WebserverTest and is intended
+    ///to contain all WebserverTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class WebserverTest
+    {
+
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        // 
+        //You can use the following additional attributes as you write your tests:
+        //
+        //Use ClassInitialize to run code before running the first test in the class
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Use ClassCleanup to run code after all tests in a class have run
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Use TestInitialize to run code before running each test
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        /// <summary>
+        ///A test for ResolveWebservice
+        ///</summary>
+        [TestMethod()]
+        [DeploymentItem("FortAwesomeUtil.dll")]
+        public void ResolveWebserviceTest()
+        {
+            Webserver server = new Webserver();
+            server.Bind("http://+:80/Temporary_Listen_Addresses/FortAwesomeUtil.test/base/");
+            var dummy1 = new DummyWebservice();
+            server.RegisterWebservice("foo1/", dummy1);
+            var dummy2 = new DummyWebservice();
+            server.RegisterWebservice("foo2/a/", dummy2);
+            server.RegisterWebservice("foo2/b/", dummy2);
+            server.Start();
+
+            string url = "http://127.0.0.1:80/Temporary_Listen_Addresses/FortAwesomeUtil.test/base/foo2/a/basic/";
+            Webservice expected = dummy1; // TODO: Initialize to an appropriate value
+            Webservice actual;
+            actual = server.ResolveWebservice(url);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
